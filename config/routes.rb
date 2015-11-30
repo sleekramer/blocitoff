9926ba7d7520a0121e46c6af41ac61ac30c7787a
Steven Lee-Kramer <sleekramer@gmail.com>
2015-11-30 11:32:03 -0500

    Complete user story, add items model and controller with create action, add items form and item partial for creation and display.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+
+
   authenticated :user do
     root 'users#show', as: :authenticated
   end
-
+  resources :users, only: [] do
+    resources :items, only: [:create]
+  end
   devise_for :users
   root to: 'welcome#about'
 
